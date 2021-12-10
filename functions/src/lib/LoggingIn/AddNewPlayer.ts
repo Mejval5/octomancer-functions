f07c26bd1a765533e46e72f9d4d62889d71d20a6
Wood <necesal.daniel@gmail.com>
2021-12-10 13:05:58 +0100

    fixes

--- functions/src/lib/LoggingIn/AddNewPlayer.ts
+++ functions/src/lib/LoggingIn/AddNewPlayer.ts
@@ -3,7 +3,7 @@ import * as admin from 'firebase-admin'
 import {newSinglePlayerData} from '../DataScripts/SinglePlayerData'
 import {newPotions} from '../DataScripts/PotionData'
 import {newDailyRewardData} from '../DataScripts/DailyRewardData'
-import {GetRandomBotDungeon} from '../DataScripts/LevelData'
+import {newLevelData} from '../DataScripts/LevelData'
 import {newTotem} from '../DataScripts/TotemData'
 import {createTrapData} from '../DataScripts/CreateTrapData'
 import {createItemData} from '../DataScripts/CreateItemData'
@@ -30,10 +30,10 @@ export const _addNewPlayer = functions.pubsub.topic('create-new-player').onPubli
 
 export async function AddNewPlayer (playerName: string, authToken: string) {
 
-    const levelTask = GetRandomBotDungeon()
+    //const levelTask = GetRandomBotDungeon()
     const totemTask = newTotem()
 
-    const [level, totem] = await Promise.all([levelTask, totemTask])
+    const [totem] = await Promise.all([totemTask])
 
     const playerData: playerTypeFirebase = {
         AuthToken: authToken,
@@ -53,7 +53,7 @@ export async function AddNewPlayer (playerName: string, authToken: string) {
         SinglePlayerData: newSinglePlayerData,
         PotionData: newPotions(),
         DailyRewardData: newDailyRewardData,
-        LevelData: level,
+        LevelData: newLevelData,
         TotemData: totem,
         DefenseLog: {},
         TrapData: createTrapData(),
