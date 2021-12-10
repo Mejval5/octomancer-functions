6942ee1ad1bfcd7dc89488f7ff0a91d7a6362966
Wood <necesal.daniel@gmail.com>
2021-12-10 12:00:14 +0100

    removed expereience from traps

--- functions/src/lib/Types/TrapTypes.ts
+++ functions/src/lib/Types/TrapTypes.ts
@@ -11,7 +11,6 @@ export type allTrapItemsType = {
 
 export type trapItemType = {
     TrapLevel: number;
-    TrapExperience: number;
     UpgradingBool: boolean;
     TimeStampFinishedUpgrading: admin.firestore.Timestamp;
 }
--- functions/src/lib/dataScripts/TrapData.ts
+++ functions/src/lib/dataScripts/TrapData.ts
@@ -4,7 +4,6 @@ import { trapItemType } from "../Types/TrapTypes"
 
 export const newTrapItemData: trapItemType = {  
   TrapLevel: 1,
-  TrapExperience: 0,
   UpgradingBool: false,
   TimeStampFinishedUpgrading: admin.firestore.Timestamp.now()
 }
